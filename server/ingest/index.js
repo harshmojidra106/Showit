301118fbd33fa45442e5b6ee5100718e3f324666
harshmojidra <harshmojidra106@gmail.com>
2026-02-05 23:37:28 +0530

    Fix clerk event names and email field

--- server/ingest/index.js
+++ server/ingest/index.js
@@ -3,18 +3,15 @@ import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
-//inngest function to save user data
-
 const SyncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/users.created" },
   async ({ event, step }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const UserData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + "" + last_name,
+      name: first_name + " " + last_name,
       image: image_url,
     };
     await User.create(UserData);
@@ -23,23 +20,21 @@ const SyncUserCreation = inngest.createFunction(
 
 const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-with-clerk" },
-  { event: "clerk/user.deleted" },
+  { event: "clerk/users.deleted" }, // Fixed: added 's'
   async ({ event }) => {
     const { id } = event.data;
-
     await User.findByIdAndDelete(id);
   },
 );
+
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
-  { event: "clerk/user.updated" },
+  { event: "clerk/users.updated" }, // Fixed: added 's'
   async ({ event }) => {
-    const { id, first_name, last_name, email_addresses, image_url } =
-      event.data;
+    const { id, first_name, last_name, email_addresses, image_url } = event.data;
     const UserData = {
-      _id: id,
-      email: email_addresses[0].email_addresses,
-      name: first_name + "" + last_name,
+      email: email_addresses[0].email_address, // Fixed: removed extra 's'
+      name: first_name + " " + last_name, // Fixed: added space
       image: image_url,
     };
     await User.findByIdAndUpdate(id, UserData, { new: true });
@@ -47,4 +42,3 @@ const syncUserUpdation = inngest.createFunction(
 );
 
 export const functions = [SyncUserCreation, syncUserDeletion, syncUserUpdation];
-
