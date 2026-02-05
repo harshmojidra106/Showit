932890e2b0915c0e26bfbe12a2267507e2c51b30
harshmojidra <harshmojidra106@gmail.com>
2026-02-05 23:23:16 +0530

    Fix clerk event names and email field

--- server/ingest/index.js
+++ server/ingest/index.js
@@ -3,49 +3,57 @@ import User from "../models/User.js";
 
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
-//inngest function to save user data
-
+// CREATE USER
 const SyncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
   { event: "clerk/users.created" },
-  async ({ event, step }) => {
+  async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
+
     const UserData = {
       _id: id,
       email: email_addresses[0].email_address,
-      name: first_name + "" + last_name,
+      name: `${first_name} ${last_name}`,
       image: image_url,
     };
+
     await User.create(UserData);
-  },
+  }
 );
 
+// DELETE USER
 const syncUserDeletion = inngest.createFunction(
   { id: "delete-user-with-clerk" },
-  { event: "clerk/user.deleted" },
+  { event: "clerk/users.deleted" },   // ✅ FIXED
   async ({ event }) => {
     const { id } = event.data;
-
     await User.findByIdAndDelete(id);
-  },
+  }
 );
+
+// UPDATE USER
 const syncUserUpdation = inngest.createFunction(
   { id: "update-user-from-clerk" },
-  { event: "clerk/user.updated" },
+  { event: "clerk/users.updated" },   // ✅ FIXED
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
+
     const UserData = {
-      _id: id,
-      email: email_addresses[0].email_addresses,
-      name: first_name + "" + last_name,
+      email: email_addresses[0].email_address,  // ✅ FIXED
+      name: `${first_name} ${last_name}`,
       image: image_url,
     };
+
     await User.findByIdAndUpdate(id, UserData, { new: true });
-  },
+  }
 );
 
-export const functions = [SyncUserCreation, syncUserDeletion, syncUserUpdation];
+export const functions = [
+  SyncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+];
 
 // trigger redeploy
