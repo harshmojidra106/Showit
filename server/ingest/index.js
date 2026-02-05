771128d1e6909f4ee2f7ccbf74c9085f20a8c045
2201030400063 <2201030400063@silveroakuni.ac.in>
2026-02-05 14:41:29 +0530

    git commit -m "Fix clerk events and inngest integration"

--- server/ingest/index.js
+++ server/ingest/index.js
@@ -7,7 +7,7 @@ export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
 const SyncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
-  { event: "clerk/users.created" },
+  { event: "clerk/user.created" },
   async ({ event, step }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
